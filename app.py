919357429f90e205c73d543273ca6deb22c864f1
Anthony Stone <stonea13@southernct.edu>
2026-03-10 12:41:08 -0400

    some changes

--- app.py
+++ app.py
@@ -13,5 +13,13 @@ def index():
     
     return render_template("index.html", todos=todos)
 
+
+@app.route("/delete/<int:task_id>")
+def delete(task_id):
+    if 0 <= task_id < len(todos):
+        todos.pop(task_id)
+    return redirect("/")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
